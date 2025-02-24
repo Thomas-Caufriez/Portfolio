e86b062c2bfc36afa88ebc93a9d1f4507a078482
Thomas-Caufriez <thcaufriez@gmail.com>
2025-02-24 11:28:19 +0100

    return to alert update 3

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,9 @@
 // UPDATE ALERT WINDOW
+console.log("Current path: " + window.location.pathname);
 
-window.alert("This webpage is still under development, have a look at the readme on github to see where I am at!\nActual version : V0.1");
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.pathname === "/index.html" || window.location.pathname === "/"){  
+        window.alert("This webpage is still under development, have a look at the readme on github to see where I am at!\nActual version : V0.1");
 
 // FUNCTION TO CREATE PROJECTS CARDS
 
