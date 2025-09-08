34b6d76f9461249275d2b07462e983758df4a5b3
Thomas Caufriez <118296253+Thomas-Caufriez@users.noreply.github.com>
2025-09-08 11:35:03 +0200

    Added aos library

--- Assets/javascripts/main.js
+++ Assets/javascripts/main.js
@@ -3,3 +3,10 @@ console.log('main.js loaded')
 import "./skills.js";
 import "./cards.js";
 import "./sidebar.js";
+
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init({
+        once: true,
+        duration: 800,
+    });
+});
