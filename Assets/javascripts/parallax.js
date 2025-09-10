4ca88dc983e3614575755c1d6d91db4ed43d22b9
Thomas Caufriez <118296253+Thomas-Caufriez@users.noreply.github.com>
2025-09-10 11:46:21 +0200

    Parallax for mobile

--- Assets/javascripts/parallax.js
+++ Assets/javascripts/parallax.js
@@ -1,22 +1,41 @@
 console.log('parallax.js loaded');
 
-const grid = document.getElementById("grid");
-const sun = document.getElementById("sun");
-const palms = document.getElementById("palms");
-const horizon = document.getElementById("horizon");
-const stars = document.getElementById("stars");
-const text = document.getElementById("introText");
+const isPortrait = window.matchMedia("(orientation: portrait)").matches;
+
+
+
+const grid = isPortrait ? document.querySelector('#grid.portrait') : document.querySelector('#grid.landscape');
+
+
+
+const sun = isPortrait 
+  ? document.querySelector('#sun.portrait')
+  : document.querySelector('#sun.landscape');
+
+const palms = isPortrait 
+  ? document.querySelector('#palms.portrait')
+  : document.querySelector('#palms.landscape');;
+
+const horizon = isPortrait 
+  ? document.querySelector('#horizon.portrait')
+  : document.querySelector('#horizon.landscape');
+
+const stars = isPortrait 
+  ? document.querySelector('#stars.portrait')
+  : document.querySelector('#stars.landscape');
+
+const text = document.querySelector('#introText');
 
 function onScroll() {
 
     const scrollY = window.scrollY;
-    
+
     grid.style.transform = `translateY(${scrollY * 0.5}px)`;
     stars.style.transform = `translateY(${scrollY * 0.2}px)`;
     sun.style.transform = `translateY(${scrollY * 0.5}px)`;
     palms.style.transform = `translateY(${scrollY * 0.5}px)`;
     horizon.style.transform = `translateY(${scrollY * 0.5}px)`;
-    text.style.transform = `translateY(${scrollY * 0.6}px) rotate(-5deg)`;
+    text.style.transform = `translateY(${scrollY * 0.4}px) rotate(-5deg)`;
 }
 
 function loop() {
