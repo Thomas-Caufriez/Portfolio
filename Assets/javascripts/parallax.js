5805f04819ab0b27ea8f0f36b33ce98956e9855b
Thomas Caufriez <118296253+Thomas-Caufriez@users.noreply.github.com>
2025-09-10 12:06:35 +0200

    Parallax screen update
    Changed the js so it listen if the user change the screen orientation while he's on the page

--- Assets/javascripts/parallax.js
+++ Assets/javascripts/parallax.js
@@ -1,30 +1,35 @@
 console.log('parallax.js loaded');
 
-const isPortrait = window.matchMedia("(orientation: portrait)").matches;
+const screenOrientation = window.matchMedia("(orientation: portrait)");
 
+let grid, sun, palms, horizon, stars, text;
 
+function updateOrientation() {
 
-const grid = isPortrait ? document.querySelector('#grid.portrait') : document.querySelector('#grid.landscape');
+    const isPortrait = screenOrientation.matches;
 
+    grid = isPortrait 
+    ? document.querySelector('#grid.portrait') 
+    : document.querySelector('#grid.landscape');
 
+    sun = isPortrait 
+    ? document.querySelector('#sun.portrait')
+    : document.querySelector('#sun.landscape');
 
-const sun = isPortrait 
-  ? document.querySelector('#sun.portrait')
-  : document.querySelector('#sun.landscape');
+    palms = isPortrait 
+    ? document.querySelector('#palms.portrait')
+    : document.querySelector('#palms.landscape');;
 
-const palms = isPortrait 
-  ? document.querySelector('#palms.portrait')
-  : document.querySelector('#palms.landscape');;
+    horizon = isPortrait 
+    ? document.querySelector('#horizon.portrait')
+    : document.querySelector('#horizon.landscape');
 
-const horizon = isPortrait 
-  ? document.querySelector('#horizon.portrait')
-  : document.querySelector('#horizon.landscape');
+    stars = isPortrait 
+    ? document.querySelector('#stars.portrait')
+    : document.querySelector('#stars.landscape');
 
-const stars = isPortrait 
-  ? document.querySelector('#stars.portrait')
-  : document.querySelector('#stars.landscape');
-
-const text = document.querySelector('#introText');
+    text = document.querySelector('#introText');
+ }
 
 function onScroll() {
 
@@ -43,5 +48,9 @@ function loop() {
     onScroll();
     requestAnimationFrame(loop);
 }
+
+updateOrientation();
+
+screenOrientation.addEventListener('change', updateOrientation)
 
 loop()
